ee664ea7f1e8149d2eceab16f9755379ba7e708e
woooyoung <axdsw121@gmail.com>
2024-04-19 14:40:52 +0900

    장바구니에 있는 품목을 주문, 주문 후 장바구니 비우기

---
+++ src/main/java/com/koreait/exam/batch_ex_24_04/app/order/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.koreait.exam.batch_ex_24_04.app.order.repository;
+
+import com.koreait.exam.batch_ex_24_04.app.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
